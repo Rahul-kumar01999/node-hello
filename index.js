51131ae9d7b54fb8d62379cfa1181c6b04018d69
Rahul-kumar01999 <76531928+Rahul-kumar01999@users.noreply.github.com>
2021-02-19 01:28:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const port2 = process.env.PORT2 || 3001;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!gggg\n'
+  const msg = 'Hello Node!\n'
   res.end(msg);
 });
 
 const server2 = http.createServer((req, res) => {
 	res.statusCode = 200;
-	const msg2 = 'Hello again from Node!fff\n'
+	const msg2 = 'Hello again from Node!\n'
 	res.end(msg2);
 });
 
